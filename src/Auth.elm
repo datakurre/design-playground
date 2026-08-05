b37c1293f2967c2e66f8431d691b4456083fa5d7
Asko Soukka <asko.soukka@iki.fi>
2026-08-05 17:35:49 +0000

    Fix scope encoding in GitLab OAuth URL

--- src/Auth.elm
+++ src/Auth.elm
@@ -34,7 +34,7 @@ loginUrl challenge =
         ++ Url.percentEncode redirectUri
         ++ "&response_type=code"
         ++ "&state=design-playground"
-        ++ "&scope=read_user+api"
+        ++ "&scope=read_user%20api"
         ++ "&code_challenge="
         ++ challenge
         ++ "&code_challenge_method=S256"
